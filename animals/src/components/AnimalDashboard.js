bdcbaa54ac2ba19cd4d49a25743a78d6bea8a3b3
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-10-15 23:57:00 -0400

    dashboard working

--- animals/src/components/AnimalDashboard.js
+++ animals/src/components/AnimalDashboard.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from "react";
 
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+
 import AnimalForm from "./AnimalForm.js";
 import AnimalList from "./AnimalsList.js";
 
 export default function AnimalDashboard() {
     
     const [ animals, setAnimals ] = useState([]);
-    
-    // How can get fetch the data from the server when the component mounts?
-    // How can we capture and set that data to state?
+    const [update, setUpdate] = useState(false)
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get('api/animals')
+        .then((res) => {
+        setAnimals(res.data)
+        // console.log(res);
+      })
+      .catch((err) => console.log(`Error`, err))
+
+    })
 
     return(
         <div className="dash">
-            <AnimalForm animals={animals} updateAnimals={setAnimals} />
+            <AnimalForm animals={animals} updateAnimals={setAnimals} setUpdating={setUpdate} updating={update} />
             <AnimalList animals={animals} />
         </div>
     )
